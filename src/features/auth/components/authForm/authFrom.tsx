54e6333e30ab307a2a1e5149235b32d92feac1f1
biloholovskyi <51454763+biloholovskyi@users.noreply.github.com>
2022-10-05 17:45:56 +0300

    create auth token and auto login

--- src/features/auth/components/authForm/authFrom.tsx
+++ src/features/auth/components/authForm/authFrom.tsx
@@ -1,4 +1,4 @@
-import { FC, FormEvent } from 'react'
+import { FC, FormEvent, useEffect } from 'react'
 
 import { useAppDispatch } from '../../../../hooks/redux'
 
@@ -12,6 +12,23 @@ import { FormWrapper } from './styles'
 const AuthForm: FC = (): JSX.Element => {
   const dispatch = useAppDispatch()
 
+  useEffect(() => {
+    checkToken()
+  }, [])
+
+  /*
+   * check token in local store if issues redirect to cases page else render auth form
+   * */
+  const checkToken = (): void => {
+    const token = localStorage.getItem('token-white-head-admin')
+    if (token) {
+      dispatch(loginUser(true))
+    }
+  }
+
+  /*
+   * create user data in store and crate local token
+   * */
   const signIn = (e: FormEvent<HTMLFormElement>): void => {
     e.preventDefault()
     const target = e.target as HTMLFormElement
@@ -20,10 +37,20 @@ const AuthForm: FC = (): JSX.Element => {
       target.email.value === process.env.REACT_APP_EMAIL &&
       target.password.value === process.env.REACT_APP_PASSWORD
     ) {
+      createToken()
       dispatch(loginUser(true))
     } else {
       alert('error')
     }
+  }
+
+  /*
+   * create token and save to local store
+   * */
+  const createToken = (): void => {
+    const token = 'sign-in-token'
+
+    localStorage.setItem('token-white-head-admin', JSON.stringify(token))
   }
 
   return (
