feaa52669eeb23d43d62c3695856ed96cec6b297
biloholovskyi <51454763+biloholovskyi@users.noreply.github.com>
2022-10-05 17:54:16 +0300

    log out

---
+++ src/components/header/header.tsx
@@ -0,0 +1,27 @@
+import { useAppDispatch } from '../../hooks/redux'
+
+import { loginUser } from '../../store/slices/user.slice'
+
+import DefaultButton from '../UI/buttons/defaultButton/defaultButton'
+
+import { HeaderWrapper } from './styled'
+
+const Header = (): JSX.Element => {
+  const dispatch = useAppDispatch()
+
+  /*
+   * delete user from store and redirect to auth form and delete auth token from local store
+   * */
+  const signOut = (): void => {
+    localStorage.removeItem('token-white-head-admin')
+    dispatch(loginUser(false))
+  }
+
+  return (
+    <HeaderWrapper>
+      <DefaultButton onClick={signOut}>Sign Out</DefaultButton>
+    </HeaderWrapper>
+  )
+}
+
+export default Header
--- src/pages/cases.tsx
+++ src/pages/cases.tsx
@@ -1,8 +1,11 @@
 import { FC } from 'react'
 
+import Header from '../components/header/header'
+
 const Cases: FC = (): JSX.Element => {
   return (
     <>
+      <Header />
       <h2>Cases page</h2>
     </>
   )
